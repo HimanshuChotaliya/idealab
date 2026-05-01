3958f1f868507bdafb58905e014803f723f25c5a
Himanshu Chotaliya <himanshu746h@gmail.com>
2026-05-01 23:17:10 +0530

    fix chunk size warning

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Increase warning limit to 2000kb
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          map: ['leaflet', 'react-leaflet'],
+          chart: ['recharts'],
+          mqtt: ['mqtt'],
+          qr: ['html5-qrcode', 'qrcode']
+        }
+      }
+    }
+  },
   server: {
     port: 5173,
     proxy: {
